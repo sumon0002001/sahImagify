7d35863c17b768039acfc5904eda5509d7399885
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-03-02 15:05:13 +0600

    Add mobile version

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,11 @@
+import Mobilenav from "@/components/shared/Mobilenav";
 import Sidebar from "@/components/shared/Sidebar";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
+      <Mobilenav />
 
       <div className="root-container">
         <div className="wrapper">{children}</div>

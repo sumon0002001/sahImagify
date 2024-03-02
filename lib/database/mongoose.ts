d9a474dcb4c2e8ceaf0bf4f34e810b8d72957bae
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-03-02 15:46:27 +0600

    add databse

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,28 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URI;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+export const dbConnect = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URL) throw new Error("Please add your Mongo URI to .env.local");
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "sahreenImagify",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
